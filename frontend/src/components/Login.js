efbdedfd3ab3a559bb9096d5114b77befa87877b
Rinaldi20 <rinaldi.naro@gmail.com>
2022-06-07 13:28:12 +0700

    frontend login

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [msg, setMsg] = useState('');
+    const navigate = useNavigate();
+
+    const Auth = async(e) =>{
+        e.preventDefault();
+        try {
+            await axios.post ('http://localhost:5000/login', {
+               
+                email: email,
+                password: password,
+                
+            });
+            navigate('/dashboard');
+        } catch (error) {
+             if(error.response){
+                 setMsg(error.response.data.msg);
+             }
+        }    
+    }
+  return (
+    <section className="hero has-background-grey-light is-success is-fullheight is-fullwidth">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns">
+              <div className="column is-4-desktop">
+                  <form onSubmit={Auth} className=' box'>
+                       <p className='has-text-centered'>{msg}</p>
+                      <div className="fields mt-5">
+                          <label className="label">Email or Username</label>
+                          <div className="control">
+                              <input type="text" className="input" placeholder='Username'
+                              value={email} onChange={(e) => setEmail(e.target.value)}/>
+                          </div>
+                      </div>
+                      <div className="fields mt-5">
+                          <label className="label">Password</label>
+                          <div className="control">
+                              <input type="password" className="input" placeholder='**********'
+                              value={password} onChange={(e) => setPassword(e.target.value)}/>
+                          </div>
+                      </div>
+                      <div className="fields mt-5">
+                         <button className='button is-success is-fullwidth'>Login</button>
+                      </div>
+                  </form>
+              </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Login
